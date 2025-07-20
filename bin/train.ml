71c495289f43f9a6a98a64f628bd9262ea9706c4
KengoA <20113339+KengoA@users.noreply.github.com>
2025-07-20 10:02:15 +0100

    make training configurable

--- bin/train.ml
+++ bin/train.ml
@@ -16,28 +16,57 @@ let parallel_map f lst n_workers =
   Array.iter Domain.join domains ;
   Array.to_list (Array.map Option.get results)
 
+let input_xml = ref "data/train/data.xml"
+let output_txt = ref "data/train/data.txt"
+let embed_dim = ref 150
+let window_size = ref 15
+let neg_samples = ref 5
+let epochs = ref 2
+let batch_size = ref 1000
+let chunk_size_mb = ref 100
+
+let speclist =
+  [
+    ("--input", Arg.Set_string input_xml, " Input XML file path");
+    ("--output", Arg.Set_string output_txt, " Output text file path");
+    ("--embed-dim", Arg.Set_int embed_dim, " Embedding dimension");
+    ("--window-size", Arg.Set_int window_size, " Context window size");
+    ("--neg-samples", Arg.Set_int neg_samples, " Number of negative samples");
+    ("--epochs", Arg.Set_int epochs, " Number of training epochs");
+    ("--batch-size", Arg.Set_int batch_size, " Batch size for training");
+    ("--chunk-size", Arg.Set_int chunk_size_mb, " Chunk size in MB for processing");
+  ]
+
 let () =
-  (* convert from XML to txt *)
-  let input_xml = "data/train/data.xml" in
-  let output_txt = "data/train/data.txt" in
-  Printf.printf "[INFO] Converting %s to %s by stripping XML tags...\n"
-    input_xml output_txt ;
+  Arg.parse speclist (fun _ -> ()) usage_msg ;
+  
+  Printf.printf "[INFO] Training Configuration:\n" ;
+  Printf.printf "  Input XML: %s\n" !input_xml ;
+  Printf.printf "  Output TXT: %s\n" !output_txt ;
+  Printf.printf "  Embedding dimension: %d\n" !embed_dim ;
+  Printf.printf "  Window size: %d\n" !window_size ;
+  Printf.printf "  Negative samples: %d\n" !neg_samples ;
+  Printf.printf "  Epochs: %d\n" !epochs ;
+  Printf.printf "  Batch size: %d\n" !batch_size ;
+  Printf.printf "  Chunk size: %dMB\n" !chunk_size_mb ;
   flush stdout ;
-  Preprocess.Xml_utils.xml_to_txt ~input_xml ~output_txt ;
+
+  Printf.printf "[INFO] Converting %s to %s by stripping XML tags...\n"
+    !input_xml !output_txt ;
+  flush stdout ;
+  Preprocess.Xml_utils.xml_to_txt ~input_xml:!input_xml ~output_txt:!output_txt ;
   Printf.printf "[INFO] Conversion complete.\n" ;
   flush stdout ;
 
-  (* tokenize and build vocab *)
-  let chunk_size_mb = 100 in
   let all_tokens = ref [] in
   let chunk_files = ref [] in
   Printf.printf
     "[INFO] Splitting and processing file in %dMB chunks (using only first \
      chunk)...\n"
-    chunk_size_mb ;
+    !chunk_size_mb ;
   flush stdout ;
   chunk_files :=
-    Io.File_utils.read_chunks ~filename:output_txt ~chunk_size_mb
+    Io.File_utils.read_chunks ~filename:!output_txt ~chunk_size_mb:!chunk_size_mb
       ~f:(fun chunk_text ->
         flush stdout ;
         let tokens = Preprocess.Text_utils.tokenize chunk_text in
@@ -75,21 +104,16 @@ let () =
   Printf.printf "[INFO] Vocabulary built. Vocab size: %d\n" vocab_size ;
   flush stdout ;
 
-  (* train embedding *)
-  let embed_dim = 150 in
-  let window_size = 15 in
   let neg_table_size = 1_000_000 in
-  let neg_samples = 5 in
-  let epochs = 2 in
   Printf.printf
     "[INFO] Creating SkipGram model (vocab_size=%d, embed_dim=%d)...\n"
-    vocab_size embed_dim ;
+    vocab_size !embed_dim ;
   flush stdout ;
-  let model = Wordvec.SkipGram.create ~vocab_size ~embed_dim in
+  let model = Wordvec.SkipGram.create ~vocab_size ~embed_dim:!embed_dim in
   Printf.printf "[INFO] Generating training pairs...\n" ;
   flush stdout ;
   let pairs =
-    Preprocess.Text_utils.generate_pairs tokens window_size vocab_tbl
+    Preprocess.Text_utils.generate_pairs tokens !window_size vocab_tbl
   in
   Printf.printf "[INFO] Training pairs generated: %d\n" (Seq.length pairs) ;
   flush stdout ;
@@ -101,22 +125,22 @@ let () =
   in
   Printf.printf
     "[INFO] Negative sampling table built (size=%d, neg_samples=%d)\n"
-    neg_table_size neg_samples ;
+    neg_table_size !neg_samples ;
   flush stdout ;
 
-  Printf.printf "[INFO] Starting training...\n" ;
+  Printf.printf "[INFO] Starting batched training (batch_size=%d)...\n" !batch_size ;
   flush stdout ;
-  Wordvec.SkipGram.train model pairs ~neg_table ~neg_samples ~epochs ;
+  Wordvec.SkipGram.train model pairs ~neg_table ~neg_samples:!neg_samples ~epochs:!epochs ~batch_size:!batch_size () ;
   Printf.printf "[INFO] Training complete.\n" ;
-  Printf.printf "Trained word2vec model with vocab_size=%d, embed_dim=%d\n"
-    vocab_size embed_dim ;
+  Printf.printf "Trained word2vec model with vocab_size=%d, embed_dim=%d, batch_size=%d\n"
+    vocab_size !embed_dim !batch_size ;
 
   Printf.printf "[INFO] Saving vocabulary frequencies...\n" ;
   Preprocess.Text_utils.save_vocab_freq
     ~filename:"data/artifacts/vocab_freq.csv" ~sorted_vocab ;
   Printf.printf "[INFO] Saving embeddings...\n" ;
   Wordvec.SkipGram.save ~filename:"data/artifacts/embeddings.csv" ~model
-    ~embed_dim ~sorted_vocab ~vocab_tbl ;
+    ~embed_dim:!embed_dim ~sorted_vocab ~vocab_tbl ;
 
   Printf.printf "[INFO] Cleaning up chunk files...\n" ;
   Io.File_utils.cleanup_chunks !chunk_files ;
